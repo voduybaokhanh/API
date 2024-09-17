0f54dd3bffce103bdb095ffe873ea1db69b27cdc
Phong908 <122242560+khanhvo908@users.noreply.github.com>
2024-09-17 13:46:36 +0700

    1

--- routes/product.js
+++ routes/product.js
@@ -33,4 +33,41 @@ router.post("/data", function (req, res) {
     res.json({ ten: name, tuoi: age });
 });
 
+//them vao list
+//localhost:3000/product/add
+router.post("/add", function (req, res) {
+    try {
+        const { id, name, age } = req.body;
+        list.push({ id, name, age });
+        res.json({ status: true, message: "them thanh cong" });
+    } catch (error) {
+        res.json({ status: false, message: "them that bai" });
+    }
+});
+
+//lay chi tiet trong list
+//localhost:3000/product/detail
+router.get("/detail", function (req, res) {
+    try {
+        const { id } = req.query;
+        var item = list.find(p => p.id == id);
+        res.json({ status: true, data: item });
+    } catch (error) {
+        res.json({ status: false, message: "khong tim thay" });
+    }
+});
+
+//sua chi tiet trong list
+//localhost:3000/product/edit
+router.put("/edit", function (req, res) {
+    try {
+        const { id, name, age } = req.body;
+        var item = list.find(p => p.id == id);
+        item.name = name;
+        item.age = age;
+        res.json({ status: true, message: "sua thanh cong" });
+    } catch (error) {
+        res.json({ status: false, message: "sua that bai" });
+    }
+});
 module.exports = router;
