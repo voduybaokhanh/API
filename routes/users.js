cf3eaa669ca2960c8e2b1dd1cb383d53818de510
Phong908 <122242560+khanhvo908@users.noreply.github.com>
2024-10-08 20:54:01 +0700

    thêm db mới

--- routes/users.js
+++ routes/users.js
@@ -111,7 +111,7 @@ router.post('/add', async function (req, res, next) {
         await usersRouter.create(addItem);
         res.json({ status: true, message: "Thêm thành công" });
     } catch (error) {
-        res.json({ status: false, message: "Thêm thất bại", err: err });
+        res.json({ status: false, message: "Thêm thất bại", err: error.message });
     }
 });
 
